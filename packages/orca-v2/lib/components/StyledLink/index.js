2a112f9f73bf72f29e1c3d4ae150175b48426955
Adam <adambottega@gmail.com>
2019-05-22 15:26:59 +1000

    changed styledLinks hover/focus styles again to stop block multiline UI issues

--- packages/orca-v2/lib/components/StyledLink/index.js
+++ packages/orca-v2/lib/components/StyledLink/index.js
@@ -20,22 +20,10 @@ const styleLink = LinkComponent => styled(LinkComponent)`
         ? variables.defaultSpacingHalf
         : props.marginBottom ? variables.defaultSpacing : 0};
 
-  &:after {
-    content: "";
-    position: absolute;
-    top: 100%;
-    left: 0;
-    width: 100%;
-    height: ${props => (props.bold ? "2px" : props.thickUnderline ? "2px" : "1px")};
-    background-color: transparent;
-    transition: ${variables.defaultTransition};
-  }
   &:hover,
   &:focus {
-    outline: none;
-    &:after {
-      background-color: ${props => (props.active ? theme.primary : props.white ? theme.white : theme.primary)};
-    }
+    outline: 0;
+    text-decoration: underline;
   }
 `;
 
