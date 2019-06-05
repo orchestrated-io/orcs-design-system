4ece3e67f550ffbba8be5ba40bf900f359230563
Adam Bottega <adambottega@gmail.com>
2019-06-05 18:07:40 +1000

    added font awesome brands icon package to design system

--- packages/orca-design-system/lib/styleguide/Wrapper.js
+++ packages/orca-design-system/lib/styleguide/Wrapper.js
@@ -4,10 +4,11 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fal } from "@fortawesome/pro-light-svg-icons";
 import { far } from "@fortawesome/pro-regular-svg-icons";
 import { fas } from "@fortawesome/pro-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import colours from "../colours";
 import GlobalStyles from "../globalStyles";
 
-library.add(fal, far, fas);
+library.add(fal, far, fas, fab);
 
 export default class Wrapper extends Component {
   render() {
