77a80239a6dc6e387460406238418be5b7d84afb
Sho Mukai <sho@orchestrated.io>
2019-09-13 18:58:31 +1000

    fix: move styleguide related file out of lib, add es build to dist build

--- styleguide/Wrapper.js
+++ styleguide/Wrapper.js
@@ -3,8 +3,8 @@ import { ThemeProvider } from "styled-components";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import colours from "../colours";
-import GlobalStyles from "../globalStyles";
+import colours from "../lib/colours";
+import GlobalStyles from "../lib/globalStyles";
 
 library.add(far, fas);
 
