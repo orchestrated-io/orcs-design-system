88ee9e18079aae2ad35f1a960df3c00b7866db47
emmkong <emmnuelkong@gmail.com>
2019-10-18 15:10:24 +1100

    add avatar stories and intro mdx

--- .storybook/config.js
+++ .storybook/config.js
@@ -7,10 +7,16 @@ import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import colours from "../lib/colours";
 import GlobalStyles from "../lib/globalStyles";
+import "storybook-chromatic";
 
 library.add(far, fas);
-
-configure(require.context('../lib', true, /\.stories\.(js|mdx)$/), module);
+configure(
+  [
+    require.context("../lib", true, /\.stories\.mdx$/),
+    require.context("../lib", true, /\.stories\.js$/)
+  ],
+  module
+);
 
 const ThemeDecorator = storyFn => {
   return (
@@ -22,4 +28,6 @@ const ThemeDecorator = storyFn => {
     </ThemeProvider>
   );
 };
+
+addDecorator(withA11y);
 addDecorator(ThemeDecorator);
