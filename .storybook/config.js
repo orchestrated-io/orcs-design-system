618ac2c0cb6f588558dd6c46894e1999f20c6262
emmkong <emmnuelkong@gmail.com>
2019-10-18 11:50:26 +1100

    config global themeProvider decorator

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,19 @@
-import { configure } from '@storybook/react';
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
+import { ThemeProvider } from "styled-components";
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../lib', true, /\.stories\.js$/), module);
+import { colours, GlobalStyles } from "../lib";
+
+configure(require.context("../lib", true, /\.stories\.js$/), module);
+
+const ThemeDecorator = storyFn => {
+  return (
+    <ThemeProvider theme={colours}>
+      <>
+        <GlobalStyles />
+        {storyFn()}
+      </>
+    </ThemeProvider>
+  );
+};
+addDecorator(ThemeDecorator);
