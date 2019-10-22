ca7a2bf98629585f2ba08014bcfd947fbf7245c8
Adam Bottega <adambottega@gmail.com>
2019-10-22 14:35:07 +1100

    added spanGrid prop to Divider component so it can be used as a row divider in a grid layout

--- packages/orca-v2/lib/components/Divider/index.js
+++ packages/orca-v2/lib/components/Divider/index.js
@@ -12,7 +12,8 @@ const Divider = styled.div`
       : props.inverted
       ? rgba(colours.greyDark, 0.6)
       : colours.greyLighter};
-  height: ${props => (props.thick ? "3px;" : "1px;")};
+  height: ${props => (props.thick ? "3px" : "1px")};
+  grid-column: ${props => (props.spanGrid ? "1 / -1" : "auto")};
 `;
 
 Divider.propTypes = {
@@ -21,7 +22,9 @@ Divider.propTypes = {
   /** Divider will be 3px instead of 1px */
   thick: PropTypes.bool,
   /** Divider will be a darker colour more suited for dark backgrounds */
-  inverted: PropTypes.bool
+  inverted: PropTypes.bool,
+  /** Divider will span all columns when in a grid so it can be used as a row divider */
+  spanGrid: PropTypes.bool
 };
 
 /** @component */
