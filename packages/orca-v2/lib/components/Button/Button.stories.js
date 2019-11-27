eb181d5ae323471f7e4aa5bc8cdca4cced4ea635
Maria Sereno <maria@orchestrated.io>
2019-11-27 14:38:11 +1100

    Removed arrays and keys.

--- packages/orca-v2/lib/components/Button/Button.stories.js
+++ packages/orca-v2/lib/components/Button/Button.stories.js
@@ -19,108 +19,102 @@ export default {
   }
 };
 
-export const defaultButton = () => [
-  <Button key="button-default">Default Button</Button>
-];
+export const defaultButton = () => <Button>Default Button</Button>;
 defaultButton.story = {
   name: "Default"
 };
 
-export const size = () => [
-  <Button key="button-small" small onClick={action("clicked")}>
-    Small button
-  </Button>,
-  <Button key="button-large" large onClick={action("clicked")}>
-    Large button
-  </Button>
-];
+export const size = () => (
+  <>
+    <Button small onClick={action("clicked")}>
+      Small button
+    </Button>
+    <Button large onClick={action("clicked")}>
+      Large button
+    </Button>
+  </>
+);
 size.story = {
   name: "Size"
 };
-export const alternateColours = () => [
-  <Button key="button-success" colour="success" onClick={action("clicked")}>
-    Success/green button
-  </Button>,
-  <Button key="button-danger" colour="danger" onClick={action("clicked")}>
-    Danger/red button
-  </Button>
-];
+export const alternateColours = () => (
+  <>
+    <Button colour="success" onClick={action("clicked")}>
+      Success/green button
+    </Button>
+    <Button colour="danger" onClick={action("clicked")}>
+      Danger/red button
+    </Button>
+  </>
+);
 alternateColours.story = {
   name: "Alternate colours"
 };
-export const alternateStyle = () => [
-  <Button key="button-ghost-small" small ghost onClick={action("clicked")}>
-    Small ghost button
-  </Button>,
-  <Button key="button-ghost" ghost onClick={action("clicked")}>
-    Regular ghost button
-  </Button>,
-  <Button key="button-ghost-large" large ghost onClick={action("clicked")}>
-    Large ghost button
-  </Button>
-];
+export const alternateStyle = () => (
+  <>
+    <Button small ghost onClick={action("clicked")}>
+      Small ghost button
+    </Button>
+    <Button ghost onClick={action("clicked")}>
+      Regular ghost button
+    </Button>
+    <Button large ghost onClick={action("clicked")}>
+      Large ghost button
+    </Button>
+  </>
+);
 alternateStyle.story = {
   name: "Ghost style"
 };
 
-export const fullWidthButton = () => [
-  <Button key="full-width" fullWidth>
-    Full width button
-  </Button>
-];
+export const fullWidthButton = () => (
+  <Button fullWidth>Full width button</Button>
+);
 fullWidthButton.story = {
   name: "Full-width"
 };
 
-export const disabledButton = () => [
-  <Button key="disabled" disabled>
-    Disabled buttons
-  </Button>
-];
+export const disabledButton = () => <Button disabled>Disabled buttons</Button>;
 disabledButton.story = {
   name: "Disabled"
 };
 
-export const loadingButton = () => [
-  <Button key="loading" isLoading>
-    Loading . . .
-  </Button>
-];
+export const loadingButton = () => <Button isLoading>Loading . . .</Button>;
 loadingButton.story = {
   name: "Loading"
 };
 
-export const iconButton = () => [
+export const iconButton = () => (
   <>
-    <Button key="icon-left" iconLeft>
+    <Button iconLeft>
       <Icon icon={["fas", "user-plus"]} />
       Left aligned icon
     </Button>
-    <Button key="icon-right" iconRight>
+    <Button iconRight>
       Right aligned icon
       <Icon icon={["fas", "download"]} />
     </Button>
   </>
-];
+);
 iconButton.story = {
   name: "With Icons"
 };
 
-export const iconOnlyButton = () => [
+export const iconOnlyButton = () => (
   <>
     <Flex alignStart>
-      <Button key="button-icon-small" small iconOnly>
+      <Button small iconOnly>
         <Icon icon={["far", "calendar-alt"]} />
       </Button>
-      <Button key="button-icon" iconOnly>
+      <Button iconOnly>
         <Icon icon={["far", "calendar-alt"]} />
       </Button>
-      <Button key="button-icon-large" large iconOnly>
+      <Button large iconOnly>
         <Icon icon={["far", "calendar-alt"]} />
       </Button>
     </Flex>
   </>
-];
+);
 iconOnlyButton.story = {
   name: "Icon Only"
 };
