0a5c8518fd9f8fc581da6dbece911e009fe05afe
Maria Sereno <maria@orchestrated.io>
2019-12-06 09:26:26 +1100

    Tag component ported to Storybook. Checked on Styleguidist.

---
+++ packages/orca-v2/lib/components/Tag/Tag.stories.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Tag from ".";
+import Layout from "../Layout";
+import Flex from "../Flex";
+
+export default {
+  title: "DESIGN SYSTEM|Tag",
+  decorators: [
+    storyFn => (
+      <Layout padding childChildHorizontalSpacing>
+        <Flex center>{storyFn()}</Flex>
+      </Layout>
+    )
+  ],
+  parameters: {
+    component: Tag
+  }
+};
+
+export const basicTag = () => <Tag>Basic Tag</Tag>;
+basicTag.story = {
+  name: "Basic"
+};
+
+export const selectedTag = () => (
+  <>
+    <Tag selected>devops</Tag>
+    <Tag selected>software engineering</Tag>
+    <Tag>product design</Tag>
+    <Tag>digital transformation</Tag>
+  </>
+);
+selectedTag.story = {
+  name: "Selected"
+};
+
+export const hideCrossTag = () => (
+  <>
+    <Tag selected hideCross>
+      devops
+    </Tag>
+    <Tag selected hideCross>
+      software engineering
+    </Tag>
+    <Tag>product design</Tag>
+    <Tag>digital transformation</Tag>
+  </>
+);
+hideCrossTag.story = {
+  name: "No Cross"
+};
+
+export const disabledTag = () => (
+  <>
+    <Tag disabled>devops</Tag>
+    <Tag disabled>software engineering</Tag>
+    <Tag disabled>product design</Tag>
+    <Tag disabled>digital transformation</Tag>
+  </>
+);
+disabledTag.story = {
+  name: "Disabled"
+};
