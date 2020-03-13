02894a31b5bbe1ed0c2fe2d03fd13dc8a34f5759
Maria Sereno <maria@orchestrated.io>
2020-03-13 14:30:00 +1100

    Additional fixes for using orcs-design-system in other apps. (Fixed exports)

--- packages/orcs-design-system/lib/index.js
+++ packages/orcs-design-system/lib/index.js
@@ -1,11 +1,16 @@
 // Import CSS reset and Global Styles
 export { default as systemtheme } from "./systemtheme";
+export { default as variables } from "./variables";
+export { default as colours } from "./colours";
 
+export { default as Avatar } from "./components/Avatar";
 export { default as Badge } from "./components/Badge";
 export { default as Box } from "./components/Box";
 export { default as Button } from "./components/Button";
 export { default as Card } from "./components/Card";
 export { default as Checkbox } from "./components/Checkbox";
+export { default as ContentTabs } from "./components/ContentTabs";
+export { default as DatePicker } from "./components/DatePicker";
 export { default as Dialogue } from "./components/Dialogue";
 export { default as Divider } from "./components/Divider";
 export { default as Expandable } from "./components/Expandable";
@@ -15,7 +20,7 @@ export { default as Header } from "./components/Header";
 export { default as Icon } from "./components/Icon";
 export { default as Layout } from "./components/Layout";
 export { default as Loading } from "./components/Loading";
-export { default as MobileNav } from "./components/MobileNav";
+export { default as Modal } from "./components/Modal";
 export { default as Notification } from "./components/Notification";
 export { default as Popover } from "./components/Popover";
 export { default as ProgressBar } from "./components/ProgressBar";
@@ -23,10 +28,9 @@ export { default as RadioButton } from "./components/RadioButton";
 export { default as Range } from "./components/Range";
 export { default as Select } from "./components/Select";
 export { default as Sidebar } from "./components/Sidebar";
-export { default as Spacing } from "./components/Spacing";
+export { default as Spacer } from "./components/Spacer";
+export { default as StatusDot } from "./components/StatusDot";
 export { default as StyledLink } from "./components/StyledLink";
-export { default as ContentTabs } from "./components/ContentTabs";
-export { default as LayoutTabs } from "./components/LayoutTabs";
 export { default as Tag } from "./components/Tag";
 export { default as TextInput } from "./components/TextInput";
 export { default as TextArea } from "./components/TextArea";
@@ -46,10 +50,3 @@ export {
   Quote,
   Code
 } from "./components/Typography";
-export { default as DatePicker } from "./components/DatePicker";
-export { default as Avatar } from "./components/Avatar";
-export { default as StatusDot } from "./components/StatusDot";
-export { default as Modal } from "./components/Modal";
-
-export { default as variables } from "./variables";
-export { default as colours } from "./colours";
