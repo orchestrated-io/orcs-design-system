047b5735feeaae81b11ca68e35f036118e39a759
Adam Bottega <adambottega@gmail.com>
2020-04-16 10:28:39 +1000

    updated badge component for better use of colours

--- packages/orcs-design-system/lib/components/Badge/index.js
+++ packages/orcs-design-system/lib/components/Badge/index.js
@@ -1,57 +1,48 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { get } from "lodash";
 import PropTypes from "prop-types";
 import colours from "../../colours";
 import variables from "../../variables";
 import { darken, rgba } from "polished";
-import { space, layout } from "styled-system";
+import { space, layout, color } from "styled-system";
+import { themeGet } from "@styled-system/theme-get";
 
 const Item = styled.div`
-  margin: 3px;
   ${space}
   ${layout}
-  font-size: 1.2rem;
-  font-weight: 600;
+  ${color}
+  font-size: ${themeGet("fontSizes.0")}px;
+  font-weight: ${themeGet("fontWeights.2")};
   display: inline-block;
   text-align: center;
   white-space: nowrap;
   position: relative;
   padding: 5px 10px;
   cursor: default;
-  color: ${props =>
-    props.colour && colours[props.colour]
-      ? darken(0.15, colours[props.colour])
-      : darken(0.15, colours.grey)};
-  background: ${props =>
-    props.colour && colours[props.colour]
-      ? rgba(colours[props.colour], 0.3)
-      : rgba(colours.grey, 0.3)};
   border-radius: ${variables.borderRadiusSmall};
-  ${props =>
-    props.inverted
-      ? css`
-          color: #fff;
-          background: ${props =>
-            props.colour === "success"
-              ? darken(0.1, colours.success)
-              : props.colour === "warning"
-              ? darken(0.1, colours.warning)
-              : props.colour === "danger"
-              ? darken(0.1, colours.danger)
-              : props.colour === "primaryLight"
-              ? darken(0.1, colours.primaryLight)
-              : props.colour === "primaryDark"
-              ? darken(0.1, colours.primaryDark)
-              : props.colour === "primaryDarkest"
-              ? darken(0.1, colours.primaryDarkest)
-              : darken(0.1, colours.grey)};
-        `
-      : css``}
 `;
 
-export default function Badge({ colour, inverted, children, ...props }) {
+const colourMap = {
+  default: { textColour: "greyDarker", badgeColour: "greyLighter" },
+  success: { textColour: "successDarker", badgeColour: "successLighter" },
+  warning: { textColour: "warningDarker", badgeColour: "warningLighter" },
+  danger: { textColour: "dangerDarker", badgeColour: "dangerLighter" },
+  primaryLight: {
+    textColour: "primaryDarker",
+    badgeColour: "primaryLightest"
+  },
+  primary: { textColour: "primaryDarker", badgeColour: "primaryLighter" },
+  primaryDark: { textColour: "primaryDarkest", badgeColour: "primary" }
+};
+
+export default function Badge({ variant = "default", children, ...props }) {
+  const { textColour, badgeColour } = get(colourMap, variant, {
+    textColour: "greyDarker",
+    badgeColour: "greyLighter"
+  });
   return (
-    <Item inverted={inverted} colour={colour} {...props}>
+    <Item color={textColour} bg={badgeColour} {...props}>
       {children}
     </Item>
   );
@@ -59,16 +50,14 @@ export default function Badge({ colour, inverted, children, ...props }) {
 
 Badge.propTypes = {
   /** Specifies badge colour. Colours are taken from the standard design system colours. */
-  colour: PropTypes.oneOf([
+  variant: PropTypes.oneOf([
     "success",
     "warning",
     "danger",
     "primaryLight",
-    "primaryDark",
-    "primaryDarkest"
+    "primary",
+    "primaryDark"
   ]),
-  /** Changes colours to suit a dark background */
-  inverted: PropTypes.bool,
   /** The label text on the badge is passed as a child element. */
   children: PropTypes.node
 };
