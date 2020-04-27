dce5f16347195239b213fd90c4574fab51107c88
Maria Sereno <maria@orchestrated.io>
2020-04-27 14:40:54 +1000

    Improve Spacer behaviour with Flex
    Allow Spacer to inherit flexbox props

--- packages/orcs-design-system/lib/components/Spacer/index.js
+++ packages/orcs-design-system/lib/components/Spacer/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { space, layout, compose } from "styled-system";
+import { css } from "@styled-system/css";
 import styled from "styled-components";
 import propTypes from "@styled-system/prop-types";
 
@@ -7,21 +8,36 @@ import PropTypes from "prop-types";
 
 const spaceStyles = compose(space, layout);
 
-const Item = styled("div")(spaceStyles);
+const Item = styled("div")(
+  css({
+    display: "inherit",
+    alignItems: "inherit",
+    flexDirection: "inherit",
+    flexWrap: "inherit",
+    justifyContent: "inherit"
+  }),
+  spaceStyles
+);
 
-const Spacer = ({ children, ...props }) => {
+const Spacer = ({ children, display, ...props }) => {
   const clones = React.Children.toArray(children).map(child =>
     React.cloneElement(child, { ...props })
   );
 
-  return <Item>{clones}</Item>;
+  return <Item display={display}>{clones}</Item>;
 };
 
 Spacer.propTypes = {
   ...propTypes.space,
   ...propTypes.layout,
+  /** Specifies display properties for Spacer. */
+  display: PropTypes.string,
   /** Adds margins around each child item. */
   m: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+};
+
+Spacer.defaultProps = {
+  display: "inherit"
 };
 
 export default Spacer;
