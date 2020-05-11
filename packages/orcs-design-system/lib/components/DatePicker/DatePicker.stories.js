f7e549f3bad0850cb12b174652b5e95ccddee45f
Maria Sereno <maria@orchestrated.io>
2020-05-11 19:16:10 +1000

    Add documentation for DatePicker

--- packages/orcs-design-system/lib/components/DatePicker/DatePicker.stories.js
+++ packages/orcs-design-system/lib/components/DatePicker/DatePicker.stories.js
@@ -1,14 +1,12 @@
 import React from "react";
 import DatePicker from ".";
-import Box from "../Box";
 
 import "react-dates/lib/css/_datepicker.css";
 import "react-dates/initialize";
 
 export default {
   title: "Components/DatePicker",
-  decorators: [storyFn => <Box height="400px">{storyFn()}</Box>],
-  component: DatePicker
+  includeStories: []
 };
 
 export const singleDate = () => <DatePicker single numberOfMonths={1} />;
