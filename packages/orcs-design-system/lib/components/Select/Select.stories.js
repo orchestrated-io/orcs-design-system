dba0b750ce1eea0db1cd09198c91826031a44d53
Maria Sereno <maria@orchestrated.io>
2020-05-15 11:45:02 +1000

    Fix Select props and update documentation

--- packages/orcs-design-system/lib/components/Select/Select.stories.js
+++ packages/orcs-design-system/lib/components/Select/Select.stories.js
@@ -1,10 +1,10 @@
 import Select from ".";
 import React from "react";
 import Box from "../Box";
+import Spacer from "../Spacer";
 
 export default {
   title: "Components/Select",
-  decorators: [storyFn => <Box height="250px">{storyFn()}</Box>],
   parameters: {
     component: Select
   }
@@ -18,14 +18,24 @@ const options = [
   { value: "rocky road", label: "Rocky Road" }
 ];
 
-export const simpleSelect = () => <Select options={options} />;
+export const defaultSelect = () => <Select options={options} />;
+defaultSelect.story = {
+  name: "Default"
+};
 
 export const multiSelect = () => <Select options={options} isMulti />;
+multiSelect.story = {
+  name: "Multi-select"
+};
 
-export const invertedMultiSelect = () => (
-  <>
-    <Box bg="greyDarkest" width="100%" height="100%" padding={4}>
+export const invertedSelect = () => (
+  <Box bg="greyDarkest" width="100%" height="100%" p="r">
+    <Spacer my="r">
+      <Select options={options} inverted />
       <Select options={options} inverted isMulti />
-    </Box>
-  </>
+    </Spacer>
+  </Box>
 );
+invertedSelect.story = {
+  name: "Inverted"
+};
