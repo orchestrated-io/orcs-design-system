77bbdd188360fbbb8c7a1e32adbaf7a8ee911069
Maria Sereno <maria@orchestrated.io>
2020-05-15 14:57:54 +1000

    TextInput documentation update

--- packages/orcs-design-system/lib/components/TextInput/TextInput.stories.js
+++ packages/orcs-design-system/lib/components/TextInput/TextInput.stories.js
@@ -1,60 +1,57 @@
 import React from "react";
-
-import Layout from "../Layout";
 import TextInput from ".";
+import Spacer from "../Spacer";
 
 export default {
   title: "Components/TextInput",
-  component: TextInput,
-  decorators: [
-    storyFn => (
-      <Layout padding childVerticalSpacing>
-        {storyFn()}
-      </Layout>
-    )
-  ]
+  component: TextInput
 };
 
-export const standard = () => [
-  <TextInput
-    id="textInput1"
-    key="textInput1"
-    type="text"
-    label="Full name"
-    placeholder="E.g. John Smith"
-  />,
-  <TextInput
-    id="textInput2"
-    key="textInput2"
-    type="text"
-    label="Full name"
-    placeholder="E.g. John Smith"
-    invalid
-  />,
-  <TextInput
-    id="textInput3"
-    key="textInput3"
-    type="text"
-    label="Full name"
-    placeholder="E.g. John Smith"
-    valid
-  />,
-  <TextInput
-    id="textInput4"
-    key="textInput4"
-    type="text"
-    label="Full name"
-    placeholder="E.g. John Smith"
-    mandatory
-  />
-];
+export const defaultTextInput = () => (
+  <Spacer m="r">
+    <TextInput
+      id="textInput1"
+      key="textInput1"
+      type="text"
+      label="Full name"
+      placeholder="E.g. John Smith"
+    />
+    <TextInput
+      id="textInput2"
+      key="textInput2"
+      type="text"
+      label="Full name"
+      placeholder="E.g. John Smith"
+      invalid
+    />
+    <TextInput
+      id="textInput3"
+      key="textInput3"
+      type="text"
+      label="Full name"
+      placeholder="E.g. John Smith"
+      valid
+    />
+    <TextInput
+      id="textInput4"
+      key="textInput4"
+      type="text"
+      label="Full name"
+      placeholder="E.g. John Smith"
+      mandatory
+    />
+  </Spacer>
+);
+defaultTextInput.story = {
+  name: "Default"
+};
 
 const numberProps = {
   decimalScale: 2,
   prefix: "$"
 };
 
-export const numberType = () => [
+export const numberTextInput = () => [
   <TextInput
     id="numInput1"
     key="numInput1"
@@ -63,80 +60,96 @@ export const numberType = () => [
     value="345345.55"
   />
 ];
+numberTextInput.story = {
+  name: "Number"
+};
 
-export const floatingLabel = () => [
-  <TextInput
-    id="textInput5"
-    key="textInput5"
-    type="text"
-    label="Full name"
-    placeholder="E.g. John Smith"
-    floating
-  />,
-  <TextInput
-    id="textInput6"
-    key="textInput6"
-    type="text"
-    label="Full name"
-    placeholder="E.g. John Smith"
-    floating
-    invalid
-  />,
-  <TextInput
-    id="textInput7"
-    key="textInput7"
-    type="text"
-    label="Full name"
-    placeholder="E.g. John Smith"
-    floating
-    valid
-  />,
-  <TextInput
-    id="textInput8"
-    key="textInput8"
-    type="text"
-    label="Full name"
-    placeholder="E.g. John Smith"
-    floating
-    mandatory
-  />
-];
+export const floatingLabelTextInput = () => (
+  <Spacer m="r">
+    <TextInput
+      id="textInput5"
+      key="textInput5"
+      type="text"
+      label="Full name"
+      placeholder="E.g. John Smith"
+      floating
+    />
+    <TextInput
+      id="textInput6"
+      key="textInput6"
+      type="text"
+      label="Full name"
+      placeholder="E.g. John Smith"
+      floating
+      invalid
+    />
+    <TextInput
+      id="textInput7"
+      key="textInput7"
+      type="text"
+      label="Full name"
+      placeholder="E.g. John Smith"
+      floating
+      valid
+    />
+    <TextInput
+      id="textInput8"
+      key="textInput8"
+      type="text"
+      label="Full name"
+      placeholder="E.g. John Smith"
+      floating
+      mandatory
+    />
+  </Spacer>
+);
+floatingLabelTextInput.story = {
+  name: "Floating label"
+};
 
-export const withIcons = () => [
-  <TextInput
-    id="textInput9"
-    key="textInput9"
-    type="text"
-    label="Phone number"
-    placeholder="E.g. (03) 9451 1786"
-    iconLeft={["fas", "phone"]}
-  />,
-  <TextInput
-    id="textInput10"
-    key="textInput10"
-    type="text"
-    label="Phone number"
-    placeholder="E.g. (03) 9451 1786"
-    floating
-    iconLeft={["fas", "phone"]}
-  />,
-  <TextInput
-    fullWidth
-    id="textInput11"
-    key="textInput11"
-    type="text"
-    label="What are you looking for?"
-    placeholder="E.g. agile teams"
-    iconRight={["fas", "search"]}
-  />,
-  <TextInput
-    fullWidth
-    id="textInput12"
-    key="textInput12"
-    type="text"
-    label="What are you looking for?"
-    placeholder="E.g. agile teams"
-    floating
-    iconRight={["fas", "search"]}
-  />
-];
+export const withIconsTextInput = () => (
+  <Spacer m="r">
+    <TextInput
+      id="textInput9"
+      key="textInput9"
+      type="text"
+      label="Phone number"
+      placeholder="E.g. (03) 9451 1786"
+      iconLeft={["fas", "phone"]}
+    />
+
+    <TextInput
+      id="textInput10"
+      key="textInput10"
+      type="text"
+      label="Phone number"
+      placeholder="E.g. (03) 9451 1786"
+      floating
+      iconLeft={["fas", "phone"]}
+    />
+
+    <TextInput
+      fullWidth
+      id="textInput11"
+      key="textInput11"
+      type="text"
+      label="What are you looking for?"
+      placeholder="E.g. agile teams"
+      iconRight={["fas", "search"]}
+    />
+
+    <TextInput
+      fullWidth
+      id="textInput12"
+      key="textInput12"
+      type="text"
+      label="What are you looking for?"
+      placeholder="E.g. agile teams"
+      floating
+      iconRight={["fas", "search"]}
+    />
+  </Spacer>
+);
+withIconsTextInput.story = {
+  name: "With Icons"
+}
