6b7e724dfb6f3851df31c44ee30866bfe0deee18
Adam Bottega <adambottega@gmail.com>
2020-05-19 14:53:02 +1000

    Updated status dot

--- packages/orcs-design-system/lib/components/StatusDot/index.js
+++ packages/orcs-design-system/lib/components/StatusDot/index.js
@@ -1,49 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled, { ThemeProvider } from "styled-components";
-import colours from "../../colours";
+import { space, layout, color, compose, variant } from "styled-system";
+import { css } from "@styled-system/css";
 import systemtheme from "../../systemtheme";
-import { space, layout } from "styled-system";
+import Icon from "../Icon";
 
-const StatusDotWrapper = styled.div`
-  ${space}
-  ${layout}
-  width: 15px;
-  height: 15px;
-  border-radius: 50%;
-  background-color: ${props =>
-    props.on
-      ? colours.success
-      : props.warning
-      ? colours.warning
-      : colours.danger};
-`;
+const StatusDotStyles = compose(space, layout, color);
 
-/**
- * Status dot component is used to specify a true/false, met/not met or on/off state. One, or multiple can be used to represent different things. If no properties are specified, StatusDot is rendered using the `warning` colour from the design system colours.
- *
- * The horizontal layout and tooltip functionality comes from other components (`Flex` and `Popover` respectively).
- */
+const StatusDotItem = styled("div")(
+  props =>
+    css({
+      width: props.icon ? "18px" : "10px",
+      height: props.icon ? "18px" : "10px",
+      borderRadius: "50%",
+      bg: "secondary",
+      fontSize: "1",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    }),
+  variant({
+    variants: {
+      default: {},
+      success: {
+        bg: "success"
+      },
+      warning: {
+        bg: "warning"
+      },
+      danger: {
+        bg: "danger"
+      }
+    }
+  }),
+  StatusDotStyles
+);
 
-export default function StatusDot({ theme, on, warning, ...props }) {
+export default function StatusDot({ theme, icon, ...props }) {
   return (
     <ThemeProvider theme={theme}>
-      <StatusDotWrapper on={on} warning={warning} {...props} />
+      <StatusDotItem {...props} icon={icon}>
+        {icon ? <Icon icon={icon} color="white" size="xs" /> : null}
+      </StatusDotItem>
     </ThemeProvider>
   );
 }
 
 StatusDot.propTypes = {
-  /** Specifies that this has an on/met/true (green) state */
-  on: PropTypes.bool,
-  /** Specifies that this has a warning (yellow) state */
-  warning: PropTypes.bool,
+  /** Specifies StatusDot colour. Colours are taken from the standard design system colours. */
+  variant: PropTypes.oneOf(["success", "warning", "danger"]),
+  /** Specifies an icon. */
+  icon: PropTypes.array,
   /** Specifies the system design theme. */
   theme: PropTypes.object
 };
 
 StatusDot.defaultProps = {
-  on: false,
-  warning: false,
   theme: systemtheme
 };
