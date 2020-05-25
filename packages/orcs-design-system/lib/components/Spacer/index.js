765f11da781cb0900ac5b073c5150084af13151a
Maria Sereno <maria@orchestrated.io>
2020-05-25 14:10:06 +1000

    Update Spacer component to pass props to wrapper instead of child

--- packages/orcs-design-system/lib/components/Spacer/index.js
+++ packages/orcs-design-system/lib/components/Spacer/index.js
@@ -1,33 +1,21 @@
 import React from "react";
 import { space, layout, compose } from "styled-system";
-import { css } from "@styled-system/css";
 import styled, { ThemeProvider } from "styled-components";
 import propTypes from "@styled-system/prop-types";
 import systemtheme from "../../systemtheme";
 
 const spaceStyles = compose(space, layout);
 
-const Item = styled("div")(
-  css({
-    display: "inherit",
-    alignItems: "inherit",
-    flexDirection: "inherit",
-    flexWrap: "inherit",
-    justifyContent: "inherit"
-  }),
-  spaceStyles
-);
+const SpacerBox = styled("div")(spaceStyles);
 
 const Spacer = ({ children, theme, ...props }) => {
-  const clones = React.Children.toArray(children).map(child =>
-    React.cloneElement(child, { ...props })
-  );
+  const clones = React.Children.toArray(children).map(child => (
+    <SpacerBox key={child.id} {...props}>
+      {React.cloneElement(child)}
+    </SpacerBox>
+  ));
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Item>{clones}</Item>
-    </ThemeProvider>
-  );
+  return <ThemeProvider theme={theme}>{clones}</ThemeProvider>;
 };
 
 Spacer.propTypes = {
