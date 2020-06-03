e04b4c9bb20f482cb93319b1a6afd5efac406b2e
Adam Bottega <adambottega@gmail.com>
2020-06-03 10:50:25 +1000

    added flex back into badge, as its more representative of how we use badges

--- packages/orcs-design-system/lib/components/Badge/Badge.stories.js
+++ packages/orcs-design-system/lib/components/Badge/Badge.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Box from "../Box";
 import Spacer from "../Spacer";
+import Flex from "../Flex";
 import Badge from ".";
 
 export default {
@@ -18,28 +19,32 @@ defaultBadge.story = {
 export const colourVariants = () => (
   <>
     <Box p="r">
-      <Spacer m="1">
-        <Badge>Default</Badge>
-        <Badge variant="success">Success</Badge>
-        <Badge variant="warning">Warning</Badge>
-        <Badge variant="danger">Danger</Badge>
-        <Badge variant="primaryLight">Low priority</Badge>
-        <Badge variant="primary">Medium priority</Badge>
-        <Badge variant="primaryDark">High priority</Badge>
-        <Badge variant="secondary">Secondary</Badge>
-      </Spacer>
+      <Flex>
+        <Spacer m="1">
+          <Badge>Default</Badge>
+          <Badge variant="success">Success</Badge>
+          <Badge variant="warning">Warning</Badge>
+          <Badge variant="danger">Danger</Badge>
+          <Badge variant="primaryLight">Low priority</Badge>
+          <Badge variant="primary">Medium priority</Badge>
+          <Badge variant="primaryDark">High priority</Badge>
+          <Badge variant="secondary">Secondary</Badge>
+        </Spacer>
+      </Flex>
     </Box>
     <Box p="r" bg="greyDarkest">
-      <Spacer m="1">
-        <Badge>Default</Badge>
-        <Badge variant="success">Success</Badge>
-        <Badge variant="warning">Warning</Badge>
-        <Badge variant="danger">Danger</Badge>
-        <Badge variant="primaryLight">Low priority</Badge>
-        <Badge variant="primary">Medium priority</Badge>
-        <Badge variant="primaryDark">High priority</Badge>
-        <Badge variant="secondary">Secondary</Badge>
-      </Spacer>
+      <Flex>
+        <Spacer m="1">
+          <Badge>Default</Badge>
+          <Badge variant="success">Success</Badge>
+          <Badge variant="warning">Warning</Badge>
+          <Badge variant="danger">Danger</Badge>
+          <Badge variant="primaryLight">Low priority</Badge>
+          <Badge variant="primary">Medium priority</Badge>
+          <Badge variant="primaryDark">High priority</Badge>
+          <Badge variant="secondary">Secondary</Badge>
+        </Spacer>
+      </Flex>
     </Box>
   </>
 );
