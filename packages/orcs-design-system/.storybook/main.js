c66d71d4fe6bb9b05ee0d93feab9bb14a772cbd4
Maria Sereno <maria@orchestrated.io>
2020-06-16 14:27:08 +1000

    Update Storybook, install react-docgen, update babelrc settings, fix stories glob

--- packages/orcs-design-system/.storybook/main.js
+++ packages/orcs-design-system/.storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
-  stories: ["../lib/**/*.stories.mdx", "../lib/**/*.stories.js"],
+  stories:['../lib/**/*.stories.@(mdx|js)'],
   addons: [
     "@storybook/addon-docs/",
     "@storybook/addon-actions/",
     "@storybook/addon-links/",
     "@storybook/addon-viewport/",
     "@storybook/addon-knobs/",
-    "@storybook/addon-a11y/"
+    "@storybook/addon-a11y/",
   ]
 };
