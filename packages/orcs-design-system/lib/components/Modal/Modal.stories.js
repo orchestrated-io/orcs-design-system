6a22eb5274b24afbb23ffc0ffe5ca7cfd69f6dde
Maria Sereno <maria@orchestrated.io>
2020-07-16 19:26:01 +1000

    Added Modal storysource code

--- packages/orcs-design-system/lib/components/Modal/Modal.stories.js
+++ packages/orcs-design-system/lib/components/Modal/Modal.stories.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 import Button from "../Button";
 import Modal from ".";
-import StyledLink from "../StyledLink";
-import Divider from "../Divider";
 import { H3, P } from "../Typography";
-import Box from "../Box";
 import Spacer from "../Spacer";
 import mdx from "./Modal.mdx";
 
@@ -40,7 +37,7 @@ const Basic = () => {
         <Spacer mb="r">
           <H3>Modal Title</H3>
           <P>
-            Content of the modal. Modal excepts any child components or content.
+            Content of the modal. Modal accepts any child components or content.
           </P>
         </Spacer>
       </Modal>
@@ -48,3 +45,36 @@ const Basic = () => {
   );
 };
 export const basicModal = () => <Basic />;
+basicModal.parameters = {
+  docs: {
+    source: {
+      code: `const [visible, setVisible] = useState(false);
+      const handleOnButtonClick = () => {
+        setVisible(true);
+      };
+      const handleOnClose = () => {
+        setVisible(false);
+      };
+      const handleOnConfirm = () => {
+        setVisible(false);
+      };
+      return (
+        <>
+          <Button onClick={handleOnButtonClick}>Open Modal</Button>
+          <Modal
+            visible={visible}
+            onConfirm={handleOnConfirm}
+            onClose={handleOnClose}
+          >
+            <Spacer mb="r">
+              <H3>Modal Title</H3>
+              <P>
+                Content of the modal. Modal accepts any child components or content.
+              </P>
+            </Spacer>
+          </Modal>
+        </>
+      );`
+    }
+  }
+};
