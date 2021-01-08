ea8df93246b6dd22d7fda8cd91727927ae941c1a
deividasm <deividas.m@gmail.com>
2021-01-08 19:26:34 +1100

    Add missing hidden files

---
+++ .storybook/manager.js
@@ -0,0 +1,10 @@
+import { addons } from "@storybook/addons";
+import orcsTheme from "./orcsTheme";
+import './titleAddon';
+
+addons.setConfig({
+  sidebarAnimations: true,
+  theme: orcsTheme,
+  showPanel: true,
+  panelPosition: "bottom"
+});
