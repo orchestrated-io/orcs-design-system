8699f3cb1f4870231a95ce00bfb74100262190fa
Adam Bottega <adambottega@gmail.com>
2021-01-20 15:17:22 +1100

    modified playroom config

--- playroom.config.js
+++ playroom.config.js
@@ -4,11 +4,22 @@ module.exports = {
   components: "./lib",
   outputPath: ".storybook/static/playroom",
   // snippets: "./snippets",
+  widths: [320, 375, 768, 1024, 1600],
   frameComponent: "./playroom/FrameComponent.js",
   exampleCode: `
-    <Button>
-      Hello World!
-    </Button>
+    <Box bg="greyLightest" p="r" height="100vh">
+        <Card title="Welcome to Playroom!">
+            <P mb="r">
+                Use playroom to quickly and easily construct shareable mockups using our design system.
+            </P>
+            <P mb="r">
+                To begin, just type &lt; and a list of our components will pop up, you can continue typing to refine your search.
+            </P>
+            <P>
+                For component examples and props usage see <StyledLink href="https://styleguide.orchestrated.io/?path=/docs/intro--page" target="_blank">ORCS storybook</StyledLink>.
+            </P>
+        </Card>
+    </Box>
   `,
   webpackConfig: () => ({
     module: {
