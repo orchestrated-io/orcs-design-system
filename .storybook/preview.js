1e0f39f90bbce5ae0e17915274ccfadd08a5f769
Karan Mehta <95593651+KaranAtTeamForm@users.noreply.github.com>
2022-11-17 10:28:33 +1100

    Themes added to design system (#182)
    * Themes added to design system
    
    * Fix theme issues
    
    * improved components
    
    * improved components
    
    * Fixed issue with Divider and TextArea
    
    * remove unwanted label
    
    * Fixed textarea issue
    
    * Fixed select
    
    * made some tweaks and fixes
    
    * Fix colour opacities
    
    * Fixed link font size
    
    * Fixed lint errors
    
    * Fixed lint errors
    
    * some further component tweaking
    
    * some more component tweaks
    
    * last few tweaks
    
    * last few fixes plus removing deprecated components
    
    * Fix modal theme issue
    
    * Seperated out props and styles
    
    * pass styles as props
    
    * export collapsed theme
    
    * fixed test to add new theme
    
    Co-authored-by: Adam Bottega <adambottega@gmail.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,24 +1,100 @@
 import React from "react";
 import { addDecorator, addParameters } from "@storybook/react";
-import { ThemeProvider } from "styled-components";
-import { DocsPage, DocsContainer } from "@storybook/addon-docs/blocks";
+import {
+  DocsPage,
+  DocsContainer as BaseContainer
+} from "@storybook/addon-docs/blocks";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { withA11y } from "@storybook/addon-a11y";
 import { default as GlobalStyles } from "../lib/GlobalStyles";
 import { default as systemtheme } from "../lib/systemtheme";
+import { default as systemThemeCollapsed } from "../lib/systemThemeCollapsed";
+import styled, { ThemeProvider } from "styled-components";
 
 library.add(far, fas);
 
-const ThemeDecorator = (storyFn) => {
+const FlexWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FlexItem = styled.div`
+  flex: 1;
+  margin: 0 10px;
+`;
+
+const getTheme = (context) => {
+  const theme = context.parameters.theme || context.globals.theme;
+  return theme === "default" ? systemtheme : systemThemeCollapsed;
+};
+
+const ThemeDecorator = (storyFn, context) => {
+  const theme = context.parameters.theme || context.globals.theme;
+  const storyTheme = getTheme(context);
+
+  switch (theme) {
+    case "side-by-side":
+      return (
+        <FlexWrapper>
+          <FlexItem key="default">
+            <ThemeProvider theme={systemtheme}>
+              <>
+                <GlobalStyles />
+                {storyFn()}
+              </>
+            </ThemeProvider>
+          </FlexItem>
+
+          <FlexItem key="collapsed">
+            <ThemeProvider theme={systemThemeCollapsed}>
+              <>
+                <GlobalStyles />
+                {storyFn()}
+              </>
+            </ThemeProvider>
+          </FlexItem>
+        </FlexWrapper>
+      );
+
+    default:
+      return (
+        <ThemeProvider theme={storyTheme}>
+          <>
+            <GlobalStyles />
+            {storyFn()}
+          </>
+        </ThemeProvider>
+      );
+  }
+};
+
+export const globalTypes = {
+  theme: {
+    title: "Theme",
+    description: "Theme for the components",
+    defaultValue: "default",
+    toolbar: {
+      icon: "sidebar",
+      items: [
+        { value: "default", title: "Default theme" },
+        { value: "collapsed", title: "Collapsed theme" },
+        { value: "side-by-side", title: "Side by side" }
+      ]
+    }
+  }
+};
+
+export const DocsContainer = ({ children, context }) => {
+  const storyTheme = getTheme(context);
+
   return (
-    <ThemeProvider theme={systemtheme}>
-      <>
-        <GlobalStyles />
-        {storyFn()}
-      </>
-    </ThemeProvider>
+    <BaseContainer context={context}>
+      <ThemeProvider theme={storyTheme}>{children}</ThemeProvider>
+    </BaseContainer>
   );
 };
 
